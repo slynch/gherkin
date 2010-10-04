bd03beb02701ff3776b890181687a4234a7abff9
Sean Lynch <slynch76180@yahoo.com>
2010-10-03 20:06:05 -0500

    Temporarily adding command line interface for ease of development and testing.

--- src/main/groovy/org/gherkin/Gherkin.groovy
+++ src/main/groovy/org/gherkin/Gherkin.groovy
@@ -1,3 +1,18 @@
 package org.gherkin
 
-println "Hello World!!"
+def cli = new CliBuilder(usage: 'java -jar gherkin.jar [gherkin files]')
+def options = cli.parse(args)
+def filesToParse = options.arguments()
+
+if (filesToParse[0] == null) {
+	cli.usage()
+} else {
+    for (file in filesToParse) {
+    	def fileHandle = new File (file);
+		if (!fileHandle.exists()) {
+			println file + " does not appear to exist....  Skipping"
+		} else {
+			println "Processing [" + file + "]"
+		}
+    }
+}
